f46ef49c72049e206becb262887ad7cb503de8d8
Chamira Fernando <chamira.fdo@gmail.com>
2016-12-04 23:28:31 +0100

    Initial project setup

---
+++ 304/304/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  304
+//
+//  Created by Chamira Fernando on 04/12/2016.
+//  Copyright © 2016 Chamira Fernando. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
